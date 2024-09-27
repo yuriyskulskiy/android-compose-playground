d14b382ab05d697cb9cf91f36059d3fe16b8d760
yuriyskulskiy <yuriyskulskiy@gmail.com>
2024-09-27 20:00:13 +0300

    Add starter card element list for parallax text.

--- app/src/main/java/com/skul/yuriy/composeplayground/feature/parallax/ParallaxRoute.kt
+++ app/src/main/java/com/skul/yuriy/composeplayground/feature/parallax/ParallaxRoute.kt
@@ -1,11 +1,26 @@
 package com.skul.yuriy.composeplayground.feature.parallax
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ParallaxRoute() {
@@ -14,10 +29,51 @@ fun ParallaxRoute() {
 
 @Composable
 fun ParallaxScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
+    ParallaxLazyColumn()
+}
+
+@Composable
+fun ParallaxLazyColumn() {
+    // Create a LazyListState to track the scroll position
+    val listState = rememberLazyListState()
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(16.dp),
+        state = listState,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "parallax")
+        items(20) { index ->
+            ParallaxListItem(listState)
+        }
+    }
+}
+
+@Composable
+fun ParallaxListItem(
+    listState: LazyListState,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(ratio = 4 / 3f),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(0.99f))
+            Text(
+                text = "Parallax text..",
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Spacer(modifier = Modifier.weight(0.01f))
+        }
     }
 }
