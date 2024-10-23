d8b77188f727cefe90457dc3c6d5647fd93d7667
yuriyskulskiy <yuriyskulskiy@gmail.com>
2024-10-23 17:30:10 +0300

    Bottom edge shadow:add starter screen.

---
+++ app/src/main/java/com/skul/yuriy/composeplayground/feature/bottomEdge/BottomEdgeShadowScreen.kt
@@ -0,0 +1,17 @@
+package com.skul.yuriy.composeplayground.feature.bottomEdge
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun BottomEdgeShadowScreen(){
+    Box(Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "bottom edge shadow")
+    }
+}
