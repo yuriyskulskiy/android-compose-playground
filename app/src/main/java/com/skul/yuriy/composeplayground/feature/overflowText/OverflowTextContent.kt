66618d5c060d9681d825ebf07bbe021c6eff8dbd
yuriyskulskiy <yuriyskul@gmail.com>
2026-03-13 22:27:22 +0200

    Add abstract box with frame

--- app/src/main/java/com/skul/yuriy/composeplayground/feature/overflowText/OverflowTextContent.kt
+++ app/src/main/java/com/skul/yuriy/composeplayground/feature/overflowText/OverflowTextContent.kt
@@ -1,14 +1,22 @@
 package com.skul.yuriy.composeplayground.feature.overflowText
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.modifier.ModifierLocal
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.skul.yuriy.composeplayground.R
@@ -25,6 +33,17 @@ internal fun OverflowTextContent(
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 16.dp, vertical = 16.dp),
     ) {
+
+        Spacer(Modifier.size(20.dp))
+        Box(
+            Modifier
+                .width(100.dp)
+                .height(135.dp)
+                .border(color = MaterialTheme.colorScheme.onSurfaceVariant, width = 1.dp)
+        ) {
+        }
+
+        Spacer(Modifier.size(20.dp))
         Text(
             text = "$text\n\n$text",
             style = MaterialTheme.typography.bodyMedium,
